5730dbc7cc7068daef228d9891991f693383cc5d
siddhant gore <siddhantgore111@gmail.com>
2022-01-14 20:46:43 +0530

    password hashing function added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,25 +1,55 @@
-const express=require("express");
+const express = require("express");
 const User = require("../models/User");
-const router=express.Router();
-const { body, validationResult } = require('express-validator');
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const { findOne } = require("../models/User");
+const { response } = require("express");
+const bcrypt = require('bcryptjs');
 
-//Create user using POST:"/api/auth" and This endpoint dosen't required authentication
-router.post("/",[
-    body('name',"Name must be greater than 3 chars").isLength({ min: 3 }),
-    body('email',"Enter Valid Email").isEmail(),
-    body('password',"Password is too short").isLength({ min: 5 })
-],
-(req,res)=>{
+//Create user using POST:"/api/auth/createuser" and This endpoint dosen't required authentication
+router.post(
+  "/createuser",
+  [
+    //validation of data send by user
+    body("name", "Name must be greater than 3 chars").isLength({ min: 3 }),
+    body("email", "Enter Valid Email").isEmail(),
+    body("password", "Password is too short").isLength({ min: 5 }),
+  ],
+
+  //this calback function inside router.post methode
+  async (req, res) => {
+
+    //generate error if data validation fails
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email:req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=>{res.json({error:"Email already exists"})});
-})
+    //try & catch block to handle any error during backend and database communication
+    try {
+      let user = await User.findOne({ email: req.body.email });
 
-module.exports=router;
+      //if email alredy exist then return the error & also return from the function
+      if (user) {
+        return res.status(400).json("Email already exists");
+      }
+      const salt=await bcrypt.genSalt(10);
+      const secPass=await bcrypt.hash(req.body.password,salt);
+
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+
+      res.json(user);
+    } 
+    catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some Error Occured")
+    }
+  }
+);
+
+module.exports = router;
+//code by siddhant gore
