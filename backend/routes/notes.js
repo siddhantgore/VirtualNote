f9b5dd7082e96c594e1596f0953d32b08718fcd8
siddhant gore <siddhantgore111@gmail.com>
2022-01-17 22:05:55 +0530

    updatenote endpoint addded

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -49,4 +49,31 @@ router.post(
   }
 );
 
+//ROUTE 2:-Update Note PUT:"/api/notes/updatenote": Require Authentication
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+    try {
+      const {title,description,tag}=req.body
+      //create new note object
+      const newNote={}
+      if(title){newNote.title=title}
+      if(description){newNote.description=description}
+      if(tag){newNote.tag=tag}
+
+      //find note which user want to update
+      let note =await Note.findById(req.params.id)
+      if(!note){return res.status(404).send("Not Found")}
+      if(note.user.toString()!==req.user.id){
+          return res.status(401).send("Not Allowed")
+      }
+
+      note=await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+      res.json(note)
+
+
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
 module.exports = router;
