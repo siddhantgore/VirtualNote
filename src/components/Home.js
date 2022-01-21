730e9721ffecc52484bd7c724e01adff7d76c198
siddhant gore <siddhantgore111@gmail.com>
2022-01-21 20:47:17 +0530

    Home

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Home() {
+  return <div>
+      <Navbar/>
+      <h1>This is home Component</h1>
+  </div>;
+}
+
+export default Home;
