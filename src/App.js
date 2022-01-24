3bfb6a425074e4c713949df02244de5188a91b49
siddhant gore <siddhantgore111@gmail.com>
2022-01-24 14:45:29 +0530

    ContextAPI Implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is VirtualBook</h1>
+      <NoteState>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+      </NoteState>
     </div>
   );
 }
