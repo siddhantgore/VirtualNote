29b8e963e094ad639d90c2208c70399d15b15868
siddhant gore <siddhantgore111@gmail.com>
2022-01-29 15:44:57 +0530

    input form for adding new notes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React,{useContext} from "react";
+import NoteContext from "../context/notes/noteContext";
+import Notes from "./Notes";
 
 function Home() {
-  
+
   return (
-    
+
     <div>
-        <h2>Add a Note</h2>
-        <h2>Your Notes</h2>
-      </div>
+      <br></br>
+      <h2>Add a Note</h2>
+      <div className="form">
+      <form>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Title</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="title"/>
+        </div>
+        <div className="form-group">
+          <label for="exampleInputPassword1">Description</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="description"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+      </div><br></br>
+      <h2>Your Notes</h2><br></br>
+      <Notes/>
+    </div>
   );
 }
 
