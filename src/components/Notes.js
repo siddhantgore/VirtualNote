61a40ce4663a90337117014aea944ef787a67570
siddhant gore <siddhantgore111@gmail.com>
2022-02-02 10:38:57 +0530

    functionality to fetchnotes on frontend from database

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,16 +1,23 @@
-import React,{useContext} from "react";
+import React,{useContext, useEffect} from "react";
 import NoteContext from "../context/notes/noteContext";
+import Addnote from "./Addnote";
 import NoteItem from "./NoteItem";
 
 function Notes() {
     const context=useContext(NoteContext);
-    const {notes,setNotes}=context;
+    const {notes,getNotes}=context;
+    useEffect(()=>{
+      getNotes();
+    },[])
   return (
-  <div class="noteItem">
+    <>
+    <Addnote/>
+  <div className="noteItem">
       {notes.map((note)=>{
-        return <NoteItem note={note}/>;
+        return <NoteItem key={note._id} note={note}/>;
       })}
   </div>
+  </>
   );
 }
 
