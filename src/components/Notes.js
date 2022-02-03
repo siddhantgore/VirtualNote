84d4ae91cc8431817f62e43fce78888f4bc731f9
siddhant gore <siddhantgore111@gmail.com>
2022-02-03 14:08:47 +0530

    editNote functionality added

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,23 +1,81 @@
-import React,{useContext, useEffect} from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import NoteContext from "../context/notes/noteContext";
 import Addnote from "./Addnote";
 import NoteItem from "./NoteItem";
 
+
 function Notes() {
-    const context=useContext(NoteContext);
-    const {notes,getNotes}=context;
-    useEffect(()=>{
-      getNotes();
-    },[])
+  const context = useContext(NoteContext);
+  const { notes, getNotes } = context;
+  useEffect(() => {
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+    getNotes();
+  }, []);
+
+  const ref = useRef(null)
+
+  const updateNote = (currentNote) => {
+    ref.current.click();
+    setNote({etitle:currentNote.title,edescription:currentNote.description,etag:currentNote.tag})
+  };
+  const [note, setNote] = useState({ etitle: "", edescription: "", etag: "" });
+  const handleClick = (e) => {
+    console.log("Upading Note")
+    e.preventDefault();
+  }
+
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })
+  }
+
   return (
     <>
-    <Addnote/>
-  <div className="noteItem">
-      {notes.map((note)=>{
-        return <NoteItem key={note._id} note={note}/>;
-      })}
-  </div>
-  </>
+      <Addnote />
+
+      <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <div className="newform">
+                <form>
+                  <div className="form-group">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" className="form-control" id="title" name="title" value={note.etitle} aria-describedby="emailHelp" placeholder="title" onChange={onChange} />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="description">Description</label>
+                    <input type="text" className="form-control" id="description" name="description" value={note.edescription} placeholder="description" onChange={onChange} />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="tag">Tag</label>
+                    <input type="text" className="form-control" id="tag" name="tag" value={note.etag} placeholder="tag" onChange={onChange} />
+                  </div>
+                  {/* <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button> */}
+                </form>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary" onClick={handleClick}>Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="noteItem">
+        {notes.map((note) => {
+          return (
+            <NoteItem key={note._id} updateNote={updateNote} note={note} />
+          );
+        })}
+      </div>
+    </>
   );
 }
 
