4dc41861d66137b9e409a394a45a96d851221976
siddhant gore <siddhantgore111@gmail.com>
2022-02-05 22:05:53 +0530

    updated

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -13,9 +13,6 @@ function NoteItem(props) {
           <h5 className="card-title"> {note.title}</h5>
           <p className="card-text">
             {note.description}
-            Cards are built with as little markup and styles as possible, but
-            still manage to deliver a ton of control and customization.This is
-            easily customized with our various
           </p>
           <i className="far fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id)}}></i>
           <i className="far fa-edit mx-2" onClick={()=>{updateNote(note)}}></i>
