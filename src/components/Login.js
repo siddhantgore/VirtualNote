5aad30f9e50b3c69ef668de7349484b9d8708d42
siddhant gore <siddhantgore111@gmail.com>
2022-02-16 15:47:02 +0530

    JWT authentication added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React ,{useState} from "react";
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+
+  const [credential, setcredential] = useState({email:"",password:""})
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setcredential({ ...credential, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+     body:JSON.stringify({email:credential.email,password:credential.password})
+    });
+    const json=await response.json()
+    console.log(json)
+    if(json.success){
+      //redirect
+      localStorage.setItem('token',json.auth_token)
+      navigate('/');
+    }
+  }
   return (
     <div>
     <div className="form">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="title">Email</label>
-          <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" placeholder="title" />
+          <label htmlFor="email">Email</label>
+          <input type="email" className="form-control" id="email" name="email" value={credential.email} onChange={onChange} aria-describedby="emailHelp" placeholder="email" />
         </div>
         <div className="form-group">
-          <label htmlFor="description">Password</label>
-          <input type="text" className="form-control" id="description" name="description" placeholder="description" />
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" name="password" value={credential.password} onChange={onChange} placeholder="password" />
         </div>
         <button  type="submit" className="btn btn-primary" >Login</button>
       </form>
