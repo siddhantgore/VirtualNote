148084fd39b6709ff735134328a8dd5a1379a4b3
siddhant gore <siddhantgore111@gmail.com>
2022-02-16 20:34:11 +0530

    signup feature added

---
+++ src/components/Signup.js
@@ -0,0 +1,54 @@
+import React ,{useState} from "react";
+import { useNavigate } from 'react-router-dom';
+
+function Signup() {
+
+  const [credential, setcredential] = useState({name:"",email:"",password:""})
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setcredential({ ...credential, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    const {name,email,password}=credential
+    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+     body:JSON.stringify({name,email,password})
+    });
+    const json=await response.json()
+    console.log(json)
+    if(json.success){
+      //redirect
+      localStorage.setItem('token',json.auth_token)
+      navigate('/');
+    }
+  }
+  return (
+    <div>
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+      <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="name" className="form-control" id="name" name="name"  onChange={onChange} aria-describedby="emailHelp" placeholder="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="form-control" id="email" name="email"  onChange={onChange} aria-describedby="emailHelp" placeholder="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" name="password"  onChange={onChange} placeholder="password" />
+        </div>
+        <button  type="submit" className="btn btn-primary" >Login</button>
+      </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
