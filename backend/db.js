ad2ed37206115afe4a8af008822ebe275b28817d
siddhant gore <siddhantgore111@gmail.com>
2022-03-01 21:57:53 +0530

    database shifted to cloud from localhost

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,24 @@
 const mongoose=require("mongoose");
-const mongoURI="mongodb://localhost:27017/virtualnote?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+// const mongoURI="mongodb://localhost:27017/virtualnote?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+
+const mongoURI="mongodb+srv://mongo:mongo@cluster0.ow9ic.mongodb.net/virtualnote?retryWrites=true&w=majority"
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
         console.log("Connected to Mongo Successfully");
     })
 }
+
+// const connectToMongo=()=>{
+//     mongoose.connect(mongoURI,{
+//         useNewUrlParser:true,
+//         useCreateIndex:true,
+//         useUnifiedTopology:true,
+//         useFindandModify:false
+//     }).then(()=>{
+//         console.log("Connected to Mongo Successfully");
+//     }).catch((err)=>console.log("Error No Connection"))
+// }
 
 module.exports=connectToMongo;
